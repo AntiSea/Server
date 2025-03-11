13cb6a42b650e3a6350d6630eeb7e82926f48497
unknownsea <seasoank@gmail.com>
2025-03-11 15:35:52 -0700

    init

---
+++ src/main/java/net/antisea/server/ServerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.antisea.server;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ServerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
